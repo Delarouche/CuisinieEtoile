e63c7f8e4b3cc142f706cc45c5648a7818ebd0f5
Delarouche <42813300+Delarouche@users.noreply.github.com>
2019-03-20 13:00:33 +0100

    Added Develop Branch
    Adde new Branch Develop
    changed Folder structure

---
+++ CE/title.js
@@ -0,0 +1,23 @@
+
+window.onload = function(){ 
+
+  document.getElementById('background').style.backgroundImage = "url('https://d3hvwccx09j84u.cloudfront.net/0,0/image/little-ears-pasta-22fc9b26.jpg') "
+document.getElementById('handcrafts').onmouseover = function()        
+     {        
+      document.getElementById('background').style.backgroundImage = "url('http://miragoods.com/wp-content/uploads/2016/09/DSC_3749.jpg')";   
+      console.log(hand);
+     };
+
+     document.getElementById('catering').onmouseover = function()        
+     {        
+      document.getElementById('background').style.backgroundImage = "url('https://d3hvwccx09j84u.cloudfront.net/0,0/image/little-ears-pasta-22fc9b26.jpg') ";
+      console.log(catrting); 
+    };
+
+     document.getElementById('blog').onmouseover = function()        
+     {        
+      document.getElementById('background').style.backgroundImage = "url('https://www.ethos3.com/wp-content/uploads/2015/12/story-presentation.jpg') ";
+      console.log(blog); 
+    };
+
+    };
